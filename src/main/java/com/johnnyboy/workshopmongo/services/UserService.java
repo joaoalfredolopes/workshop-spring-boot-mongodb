2afd39b33c43b1ebeee7bb4d0cdbb116430da7df
joaoalfredolopes <joaoalfredo_lopes@yahoo.com.br>
2023-12-01 16:36:25 -0300

    Commit 06 - Consultando usuario por id

--- src/main/java/com/johnnyboy/workshopmongo/services/UserService.java
+++ src/main/java/com/johnnyboy/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.johnnyboy.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.johnnyboy.workshopmongo.domain.User;
 import com.johnnyboy.workshopmongo.repository.UserRepository;
+import com.johnnyboy.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,12 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+		
 	}
 
 }
